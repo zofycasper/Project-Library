7fbf0f6399144f46f85f753db8305d2c5463fd51
Casper <zofycasper@gmail.com>
2022-09-23 17:27:03 +0800

    all feature updated

--- script.js
+++ script.js
@@ -7,6 +7,12 @@ let readChecks = document.querySelectorAll("#cardCheck");
 // get book details from user input
 let bookTitle;
 
+const button = document.querySelector(".add-book");
+
+addBookToLibrary();
+removeCard();
+changeRead();
+
 function Book() {
     // the constructor
     this.title = title;
@@ -16,91 +22,25 @@ function Book() {
 }
 
 function addBookToLibrary() {
-    // do stuff here
+    button.addEventListener("click", () => {
+        title = document.getElementById("title").value;
+        author = document.getElementById("author").value;
+        pages = document.getElementById("pages").value;
+        read = document.getElementById("read").checked;
+
+        let book = new Book(title, author, pages, read);
+        myLibrary.push(book);
+
+        console.log("myLibrary:");
+        console.log(myLibrary);
+
+        updateLibraryArea();
+
+        changeRead();
+
+        removeCard();
+    });
 }
-
-const button = document.querySelector(".add-book");
-
-button.addEventListener("click", () => {
-    title = document.getElementById("title").value;
-    author = document.getElementById("author").value;
-    pages = document.getElementById("pages").value;
-    read = document.getElementById("read").checked;
-
-    let book = new Book(title, author, pages, read);
-    myLibrary.push(book);
-
-    console.log(myLibrary);
-    libraryArea.textContent = "";
-
-    for (x in myLibrary) {
-        let card = document.createElement("div");
-        let cardCheck = document.createElement("input");
-
-        let titleDiv = document.createElement("div");
-        let authorDiv = document.createElement("div");
-        let pagesDiv = document.createElement("div");
-        let readDiv = document.createElement("div");
-
-        card.classList.add(`card`);
-        cardCheck.classList.add(`cardCheck`);
-        readDiv.classList.add(`read${x}`);
-
-        card.setAttribute("id", `card${x}`);
-        cardCheck.setAttribute("type", "checkbox");
-        cardCheck.setAttribute("id", `cardCheck${x}`);
-
-        libraryArea.appendChild(card);
-        titleDiv.textContent = `title: ${myLibrary[x].title}`;
-        authorDiv.textContent = `author: ${myLibrary[x].author}`;
-        pagesDiv.textContent = `pages: ${myLibrary[x].pages}`;
-        readDiv.textContent = `read: ${myLibrary[x].read}`;
-
-        card.appendChild(titleDiv);
-        card.appendChild(authorDiv);
-        card.appendChild(pagesDiv);
-        card.appendChild(readDiv);
-        card.appendChild(cardCheck);
-
-        document.querySelector(`#cardCheck${x}`).checked = myLibrary[x].read;
-        let checkBox = document.querySelector(`#cardCheck${x}`);
-        console.log(checkBox);
-    }
-
-    changeRead();
-
-    // readChecks = document.querySelectorAll(".card");
-    // console.log(readChecks);
-
-    // readChecks.forEach((i) => {
-    //     i.addEventListener("click", () => {
-    //         let iId = i.getAttribute("id");
-    //         console.log(iId);
-
-    //         // change read status
-
-    //         if (myLibrary[iId].read == true) {
-    //             myLibrary[iId].read = false;
-    //             document.querySelector(`.cardCheck${iId}`).checked =
-    //                 myLibrary[iId].read;
-    //         } else if (myLibrary[iId].read == false) {
-    //             myLibrary[iId].read = true;
-    //             document.querySelector(`.cardCheck${iId}`).checked =
-    //                 myLibrary[iId].read;
-    //         }
-
-    //         console.log(myLibrary[iId].read);
-
-    //         let cardId = document.getElementById(`${iId}`);
-    //         console.log("cardId:");
-    //         console.log(cardId);
-
-    //         // get read div
-    //         let oldRead = document.querySelector(`.read${iId}`);
-    //         oldRead.textContent = `read: ${myLibrary[iId].read}`;
-    //     });
-    // });
-});
 
 function changeRead() {
     readChecks = document.querySelectorAll(".cardCheck");
@@ -135,4 +75,72 @@ function changeRead() {
             oldRead.textContent = `read: ${myLibrary[iId].read}`;
         });
     });
+}
+
+function removeCard() {
+    let removes = document.querySelectorAll(".remove");
+    console.log("removes:");
+    console.log(removes);
+
+    removes.forEach((remove) => {
+        remove.addEventListener("click", () => {
+            let removeId = remove.getAttribute("id").replace(/[^0-9]/gi, "");
+            console.log("removeId:");
+            console.log(removeId);
+            let rmCard = document.getElementById(`card${removeId}`);
+            console.log("rmCard:");
+            console.log(rmCard);
+            myLibrary.splice(removeId, 1);
+
+            updateLibraryArea();
+            // libraryArea.removeChild(rmCard);
+            console.log("myLibrary:");
+            console.log(myLibrary);
+            removeCard();
+            changeRead();
+        });
+    });
+}
+
+function updateLibraryArea() {
+    libraryArea.textContent = "";
+
+    for (x in myLibrary) {
+        let card = document.createElement("div");
+        let cardCheck = document.createElement("input");
+        let removeButton = document.createElement("button");
+
+        let titleDiv = document.createElement("div");
+        let authorDiv = document.createElement("div");
+        let pagesDiv = document.createElement("div");
+        let readDiv = document.createElement("div");
+
+        card.classList.add(`card`);
+        cardCheck.classList.add(`cardCheck`);
+        readDiv.classList.add(`read${x}`);
+        removeButton.classList.add("remove");
+
+        removeButton.setAttribute("id", `remove${x}`);
+        card.setAttribute("id", `card${x}`);
+        cardCheck.setAttribute("type", "checkbox");
+        cardCheck.setAttribute("id", `cardCheck${x}`);
+
+        libraryArea.appendChild(card);
+        titleDiv.textContent = `title: ${myLibrary[x].title}`;
+        authorDiv.textContent = `author: ${myLibrary[x].author}`;
+        pagesDiv.textContent = `pages: ${myLibrary[x].pages}`;
+        readDiv.textContent = `read: ${myLibrary[x].read}`;
+        removeButton.textContent = "remove";
+
+        card.appendChild(titleDiv);
+        card.appendChild(authorDiv);
+        card.appendChild(pagesDiv);
+        card.appendChild(readDiv);
+        card.appendChild(cardCheck);
+        card.appendChild(removeButton);
+
+        document.querySelector(`#cardCheck${x}`).checked = myLibrary[x].read;
+        let checkBox = document.querySelector(`#cardCheck${x}`);
+        console.log(checkBox);
+    }
 }
